40a30e63877b1fcd706c4fe5726c60cb6a5f1663
Rafael de la Piñera <rdelapinera@gmail.com>
2015-07-09 23:15:48 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,9 +1,34 @@
 var path = require('path');
 
+//Postgres => postgres://user:password@host:port/database
+//SQLite => sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name 	= url[6] || null;
+var user 		= url[2] || null;
+var pwd 		= url[3] || null;
+var protocol 	= url[1] || null;
+var dialect 	= url[1] || null;
+var port 		= url[5] || null;
+var host 		= url[4] || null;
+var storage		= process.env.DATABASE_STORAGE;
+
 // ORM
 var Sequelize = require('sequelize');
-// SQLite
-var sequelize = new Sequelize(null, null, null, {dialect: 'sqlite',storage: 'quiz.sqlite'})
+// SQLite o Postgres
+var sequelize = new Sequelize(
+	DB_name, 
+	user, 
+	pwd, 
+	{
+		dialect: protocol,
+		protocol: protocol,
+		port: port,
+		host: host,
+		storage: storage, 	// Para SQLite (archivo .env)
+		omitNull: true 		// Para Postgres
+
+	}
+);
 // Tabla Quiz
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'))
 // Hacemos la tabla accesible
